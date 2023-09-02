e61a529b241893966ea78b90f1edb38d10a7abd7
Lezak.Li <123456789Cc>
2023-09-03 01:08:28 +0800

    Add: 规范配置文件添加彩蛋

--- demo-stock/src/main/java/edu/lingnan/stock/StockApplication.java
+++ demo-stock/src/main/java/edu/lingnan/stock/StockApplication.java
@@ -3,12 +3,16 @@ package edu.lingnan.stock;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 public class StockApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(StockApplication.class,args);
+        ConfigurableApplicationContext applicationContext =  SpringApplication.run(StockApplication.class,args);
+        String userName = applicationContext.getEnvironment().getProperty("user.name");
+        String port = applicationContext.getEnvironment().getProperty("server.port");
+        System.out.println(userName+"!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"+port);
     }
 }
