8bd48206e16aec98484146fea5ababa8ba066980
Lezak.Li <123456789Cc>
2023-09-18 23:48:14 +0800

    Add: 新增luckycore模块，修复gateway不能转发的问题

--- luckygateway/src/main/java/edu/lingnan/luckycloud/luckygateway/GatewayApplication.java
+++ luckygateway/src/main/java/edu/lingnan/luckycloud/luckygateway/GatewayApplication.java
@@ -1,4 +1,4 @@
-package edu.lingnan.gateway;
+package edu.lingnan.luckycloud.luckygateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
