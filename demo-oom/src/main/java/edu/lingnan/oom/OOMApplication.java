cbdd17d4e84610ddca461aef72fec086d62a170f
@lezak.li <Lezak.Li@bamboonetworks.com>
2023-10-23 15:26:01 +0800

    Add:新增demo模块

---
+++ demo-oom/src/main/java/edu/lingnan/oom/OOMApplication.java
@@ -0,0 +1,13 @@
+package edu.lingnan.oom;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OOMApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OOMApplication.class,args);
+    }
+}
